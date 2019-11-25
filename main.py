2fbb4f1ac2123a49eaa37c7e52f850421a51e42a
ftdneves <30662643+ftdneves@users.noreply.github.com>
2019-11-25 09:10:13 -0300

    Feat(open[50%]/save)

--- main.py
+++ main.py
@@ -22,11 +22,10 @@ class App(tk.Tk):
         #configuring and adding the menu bar
         menu=tk.Menu(self)
         file_menu=tk.Menu(menu,tearoff=0)
-        file_menu.add_command(label="New file")
+        file_menu.add_command(label="New file",command=self.clear_canvas)
         file_menu.add_command(label="Open",command=self.choose_file)
         file_menu.add_separator()
         file_menu.add_command(label="Save",command=self.save_file)
-        file_menu.add_command(label="Save as...",command=self.save_as_file)
         menu.add_cascade(label="File",menu=file_menu)
         algorithm_menu=tk.Menu(menu,tearoff=0)
         algorithm_menu.add_command(label="Is Eulerian", command=self.draw_eulerian)
@@ -286,19 +285,26 @@ class App(tk.Tk):
                 filetypes=filetypes)
         if filename:
             print(filename)
+            file = open(filename,"r+")
+            for line in file:
+                if line.startswith("Adj:"):
+                    line[5:len(line)]
+                elif line.startswith("Coord:"):
+                    line[7:len(line)]
+            file.close()
 
     def save_file(self):
-        contents=self.text.get(1.0,tk.END)
+        g = self.generate_graph()
+        contents="Adj: "
+        contents+=str(g.adjMatrix)
+        contents+="\n"
+        contents+="Coord: "
+        contents+="["
+        for vertice in self._nodes:
+            contents+=str(self.canvas.coords(vertice))
+            contents+=","
+        contents+="]"    
         new_file=fd.asksaveasfile(title="Save file",
-                    defaultextension=".txt",
-                    filetypes=(("Text files","*.txt"),))
-        if new_file:
-            new_file.write(contents)
-            new_file.close()
-
-    def save_as_file(self):
-        contents=self.text.get(1.0,tk.END)
-        new_file=fd.asksaveasfilename(title="Save file as",
                     defaultextension=".txt",
                     filetypes=(("Text files","*.txt"),))
         if new_file:
